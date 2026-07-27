89c726955ae7369d18da2b3be76ff06588658781
saef97 <155028082+saef97@users.noreply.github.com>
2026-07-27 20:44:34 +0530

    Time: 50 ms (38.44%) | Memory: 86.2 MB (28.28%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.java
+++ 474-ones-and-zeroes/ones-and-zeroes.java
@@ -1,36 +1,55 @@
 class Solution {
     int size;
-    // int c0g ;int c1g;
-    int [][][] t;
+
+    int[][][] t;
     int[] zero;
     int[] one;
 
-    int solve(String[] strs, int i, int m, int n) {
-        //int c0 = 0;
-       // int c1 = 0;
-       if(t[i][m][n] != -1)return t[i][m][n];
-        if (i == size)
-            return t[i][m][n] = 0;
+    int solve(String[] strs, int m, int n) {
 
-        if (zero[i] <= m && one[i] <= n) {
-            // m-=zero[i];
-            //m-=one[i];
-            return t[i][m][n] = Math.max(1 + solve(strs, i + 1, m - zero[i], n - one[i]), solve(strs, i + 1, m, n));
-        } else {
-            return t[i][m][n] = solve(strs, i + 1, m, n);
+        // if (t[][m][n] != -1)
+        //     return t[i][m][n];
+        // if (i == size)
+        //     return t[i][m][n] = 0;
+
+        // if (zero[i] <= m && one[i] <= n) {
+        //     return t[i][m][n] = Math.max(1 + solve(strs, i + 1, m - zero[i], n - one[i]), solve(strs, i + 1, m, n));
+        // } else {
+        //     return t[i][m][n] = solve(strs, i + 1, m, n);
+        // }
+
+       for (int i = size - 1; i >= 0; i--) {
+
+    for (int zeros = 0; zeros <= m; zeros++) {
+
+        for (int ones = 0; ones <= n; ones++) {
+
+            t[i][zeros][ones] = t[i + 1][zeros][ones];
+
+            if (zero[i] <= zeros && one[i] <= ones) {
+
+                t[i][zeros][ones] = Math.max(
+                    t[i][zeros][ones],
+                    1 + t[i + 1][zeros - zero[i]][ones - one[i]]
+                );
+            }
         }
+    }
+}
+        return t[0][m][n];
+
     }
 
     public int findMaxForm(String[] strs, int m, int n) {
         size = strs.length;
-        // c0g = 0;c1g =0;
+
         zero = new int[size];
         one = new int[size];
-        t = new int [size+1][m+1][n+1];
-        for(int i = 0;i<size;i++){
-            for(int j = 0;j<=m;j++)
-            Arrays.fill(t[i][j],-1);
-        }
+        t = new int[size + 1][m + 1][n + 1];
+        // for (int i = 0; i < size; i++) {
+        //     for (int j = 0; j <= m; j++)
+        //         Arrays.fill(t[i][j], -1);
+        // }
 
         for (int i = 0; i < size; i++) {
             int c0 = 0;
@@ -39,15 +58,15 @@ class Solution {
             for (char c : curr.toCharArray()) {
                 if (c == '0') {
                     c0++;
-                    // c0g++;
+
                 } else {
                     c1++;
-                    // c1g++;
+
                 }
             }
             zero[i] = c0;
             one[i] = c1;
         }
-        return solve(strs, 0, m, n);
+        return solve(strs, m, n);
     }
 }

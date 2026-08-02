35bd9fa818bc90c3ff48443e581d950737edd764
saef97 <155028082+saef97@users.noreply.github.com>
2026-08-02 18:13:55 +0530

    Time: 1 ms (83.14%) | Memory: 42.8 MB (37.38%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -2,24 +2,24 @@ class Solution {
     public boolean wordPattern(String pattern, String s) {
         int n = pattern.length();
         String [] strs = s.split(" ");
-        Map<Character,String> map = new HashMap<>();
+        Map<Character,String> map1 = new HashMap<>();
+        Map<String,Character> map2 = new HashMap<>();
+
         if(n != strs.length)return false;
         for(int i = 0;i<n;i++){
-            char curr = pattern.charAt(i);
-            String val  = strs[i];
-            if(!map.containsKey(curr)){
-                if(!map.containsValue(val)){
-                    map.put(curr,val);
-                }else{
-                    return false;
-                }
-
-            }else if(map.containsKey(curr)){
-                if(!map.get(curr).equals(val)){
-                    return false;
-                }
+            char c = pattern.charAt(i);
+            String st = strs[i];
+            if(map1.containsKey(c) && !map1.get(c).equals(st)){
+                return false;
             }
+            if(map2.containsKey(st) && !map2.get(st).equals(c)){
+                return false;
+            }
+            map1.put(c,st);
+            map2.put(st,c);
         }
         return true;
+        
+        //return true;
     }
 }

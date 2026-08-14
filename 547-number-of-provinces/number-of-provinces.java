dc9019b1bfd85211e54008f8ffefc54c66cf6257
saef97 <155028082+saef97@users.noreply.github.com>
2026-08-14 16:29:21 +0530

    Time: 2 ms (41.24%) | Memory: 47.4 MB (44.12%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,58 +1,34 @@
 class Solution {
-    class dsu{
-        int [] parents;
-        int [] rank;
-        dsu(int n){
-           // this.n = n;
-            parents = new int[n];
-            rank = new int[n];
-            for(int i = 0;i<n;i++){
-                parents[i] = i;
-                rank[i] = 0;
-            }
-        }
-        int find(int x){
-            if(parents[x] != x){
-                parents[x] = find(parents[x]);
-            }
-            return parents[x];
-        }
-        void union(int x,int y){
-            int rootX = find(x);
-            int rootY = find(y);
-            if(rootX == rootY)return ;
+    void bfs(int source, int[][] mat, boolean[] visited) {
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        q.offer(source);
+        visited[source] = true;
 
-            if(rank[rootX ]< rank[rootY]){
-                parents[rootY] = rootX;
-            }else if (rank[rootX]>rank[rootY]){
-                parents[rootX] = rootY;
-            }else{
-                parents[rootY] = rootX;
-                rank[rootX]++;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            for (int j = 0; j < mat.length; j++) {
+                if (mat[node][j] == 1 && !visited[j]) {
+                    visited[j] = true;
+                    q.offer(j);
+                }
+
             }
-            
         }
+
     }
+
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        dsu ds = new dsu(n);
-        int c =0;
-        for(int i = 0;i<n;i++){
-            for(int j= 0;j<n;j++){
-                if(isConnected[i][j] == 1){
-                    ds.union(i,j);
+        boolean[] visited = new boolean[n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i]) {
+                    count++;
+                    bfs(i, isConnected, visited);
                 }
-                
             }
-            
         }
-        Set<Integer> set = new HashSet<>();
-        for(int i = 0;i<n;i++){
-            set.add(ds.find(i));
-        }
-        return set.size();
-        
-
-        
+        return count;
     }
 }

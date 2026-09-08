5abaa042ca74feeca6081c06bdadfdf8c893842f
saef97 <155028082+saef97@users.noreply.github.com>
2026-09-08 06:28:24 +0530

    Time: 5 ms (29.24%) | Memory: 51.6 MB (82.56%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,30 +1,41 @@
 class Solution {
-    void dfs(char[][] grid,int i,int j){
+    void bfs(char[][] grid,int i,int j){
         int [][]dir = {{0,-1},{-1,0},{1,0},{0,1}};
-        int m = grid.length;
+         int m = grid.length;
         int n = grid[0].length;
-        if(i<0||i>=m || j<0 ||j>=n){
-            return;
-        }
-        if(grid[i][j] == '0')return ;
-        if(grid[i][j] == '2')return;
-        grid[i][j] = '2';
-        //if
-        for(int []d:dir){
-            int newI = i+d[0];
-            int newJ = j+d[1];
-            dfs(grid,newI,newJ);
+        Deque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{i,j});
+        while(!q.isEmpty()){
+            int [] curr = q.poll();
+            int oldi =curr[0];
+            int oldj = curr[1];
+
+            for(int []d:dir){
+                int newI = oldi+d[0];
+                int newJ = oldj+d[1];
+                if(newI<0 || newJ<0 ||newI >=m || newJ >=n)
+                continue;
+                if(grid[newI][newJ] == '0')continue;
+                if(grid[newI][newJ] == '1'){
+                    grid[newI][newJ] = '2';
+                    q.offer(new int[]{newI,newJ});
+                }
+                
+
+
+            } 
         }
         
     }
 
     public int numIslands(char[][] grid) {
-        int count = 0; int m = grid.length;
+        int count = 0;
+        int m = grid.length;
         int n = grid[0].length;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
+                    bfs(grid, i, j);
                     count++;
                 }
 

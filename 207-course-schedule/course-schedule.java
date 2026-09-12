86f5c6bfe3a04b3c722c967aa3ed1d6043cc083e
saef97 <155028082+saef97@users.noreply.github.com>
2026-09-13 02:56:33 +0530

    Time: 7 ms (56.70%) | Memory: 46.9 MB (77.48%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,42 +1,40 @@
 class Solution {
-    List<List<Integer>> adj;
-    int[] state;
-
-    boolean hasACycleDfs(int node) {
-        state[node] = 1;
-        for (int num : adj.get(node)) {
-            if (state[num] == 1)
-                return true;
-            if (state[num] == 0) {
-                if (hasACycleDfs(num))
-                    return true;
-            }
-        }
-        state[node] = 2;
-        return false;
-    }
-
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        adj = new ArrayList<>();
-        int n = numCourses;
+        List<List<Integer>> adj = new ArrayList<>();
+        int n  = numCourses;
 
-        for (int i = 0; i < n; i++) {
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        //detect a cycle;//using kahns->inorder+visited
+        int [] inDegree = new int[n];
+        for(int i = 0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        for (int i = 0; i < prerequisites.length; i++) {
+        for(int i  = 0;i<prerequisites.length;i++){
             int u = prerequisites[i][1];
             int v = prerequisites[i][0];
 
             adj.get(u).add(v);
+            inDegree[v]++;
         }
-        state = new int[n];
-        for (int i = 0; i < n; i++) {
-            if (state[i] == 0) {
-                if (hasACycleDfs(i))
-                    return false;
+        for(int i = 0;i<n;i++){
+            if(inDegree[i] == 0){
+                q.offer(i);
+                
             }
+        }int processedCount = 0;
+        while(!q.isEmpty()){
+            
+            int curr = q.poll();
+            processedCount++;
+            for(int num:adj.get(curr)){
+                inDegree[num]--;
+                if(inDegree[num] == 0){
+                    q.offer(num);
+                }
+            }
+            //visited
         }
-        return true;
+        return processedCount == n;
 
     }
 }

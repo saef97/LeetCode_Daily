93ee642236f793441c73c0521ddf3e601fe45915
saef97 <155028082+saef97@users.noreply.github.com>
2026-09-13 21:27:59 +0530

    Time: 8 ms (29.14%) | Memory: 45.6 MB (22.56%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,41 @@
+class Solution {
+     int[][][]t;
+
+    int recur(int i, int canBuy, int k,int[] price) {
+        int n = price.length;
+        if(k == 0)return 0;
+        if (i == n)
+            return t[i][canBuy][k] = 0;//directly bhi return kar do return 0;
+
+        if (t[i][canBuy][k] != -1)
+            return t[i][canBuy][k];
+        if (canBuy == 1) {
+            int buy = -price[i] + recur(i + 1, 0,k, price);
+            int skip = recur(i + 1, 1,k, price);
+            return t[i][canBuy][k] = Math.max(buy, skip);
+        } else {//canBuy == 0
+            int sell = price[i] + recur(i + 1, 1, k-1,price);
+            int skip = recur(i + 1, 0,k, price);
+            return t[i][canBuy][k] = Math.max(sell, skip);
+        }
+
+    }
+    public int maxProfit(int k, int[] prices) {
+       
+
+    //canbuy =1 i am emtpy handed->i can buy or skip only
+    //canbuy == 0 ..u have 1 stock to sell or skip
+   
+        t = new int[prices.length + 1][2][k+1];
+        // Arrays.fill(t,-1);
+        for (int i = 0; i < t.length; i++) {
+            for(int j =0;j<2;j++){
+                Arrays.fill(t[i][j],-1);
+            }
+        }
+        return recur(0, 1,k, prices);
+
+        //memoized me final return call recur hi hogi,,//return t[prices.length][2];
+
+    }
+}

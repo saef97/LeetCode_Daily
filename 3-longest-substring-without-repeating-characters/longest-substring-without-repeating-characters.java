8416f6583737ee4e10efe2b34417211c63cfa65c
saef97 <155028082+saef97@users.noreply.github.com>
2026-09-15 13:03:03 +0530

    Time: 112 ms (6.78%) | Memory: 47.5 MB (74.47%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,21 +1,23 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int i = 0;int j = 0;
-        int n = s.length();int max = 0;
-        StringBuilder sv = new StringBuilder();
+        int i = 0;int j = 0;int n = s.length();
+        if(s.isEmpty()) return 0;
+        StringBuilder sb = new StringBuilder();
+        int max = Integer.MIN_VALUE;
         while(j<n){
-            char c = s.charAt(j);
-            if(!sv.toString().contains(String.valueOf(c))){
-                sv.append(c); max = Math.max(max,sv.length());//max ko humesha banate time add kro;
-            }
-            else{
-               
-                if(!sv.isEmpty())
-                sv.deleteCharAt(0);
+            char curr = s.charAt(j);
+            if(!sb.toString().contains(String.valueOf(curr))){
+                sb.append(curr);max = Math.max(sb.length(),max);
+            }else{
+                
+                if(!sb.isEmpty())
+                sb.deleteCharAt(0);
                 i++;j--;
+                //sb.append(curr);
             }
+
             j++;
         }
-        return max;
+        return max; 
     }
 }

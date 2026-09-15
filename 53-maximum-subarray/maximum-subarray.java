f33f52628f37d5481b066a2f3a5426c6e31743e7
saef97 <155028082+saef97@users.noreply.github.com>
2026-09-15 14:19:23 +0530

    Time: 1 ms (99.92%) | Memory: 77.2 MB (48.90%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,25 +1,15 @@
 class Solution {
-    int overAllMax;
-    Integer []t;
-    int recur(int i,int[]nums){
-        if(i == nums.length-1){
-            overAllMax = Math.max(nums[i],overAllMax);
-            return nums[i];
-            };
-        if(t[i] != null)return t[i];
-        int maxAti = Math.max(nums[i],nums[i]+ recur(i+1,nums));
-
-        overAllMax = Math.max(overAllMax,maxAti);
-        return t[i]=maxAti;
-    }
     public int maxSubArray(int[] nums) {
-        //DP
-        t = new Integer[nums.length];
-        overAllMax = nums[0];
-       // Arrays.fill(t,)
-        recur(0,nums);
-
-        return overAllMax;
-        
+        int n = nums.length;
+        int currSum = 0;
+        int max = Integer.MIN_VALUE;
+        for(int num:nums){
+            currSum +=num;max =Math.max(currSum,max);
+            if(currSum <0){
+                currSum = 0;
+            }
+            
+        }
+        return max;
     }
 }
